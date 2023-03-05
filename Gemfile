97eaf40055fb2da1cb9024d45d608359125d7a3e
Armin Stiegler <armin.stiegler@icloud.com>
2023-03-05 21:58:55 +0100

    ready for heroku

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem "bootsnap", require: false
 
 gem 'bootstrap', '~> 5.2.3'
 
+gem 'will_paginate', '~> 3.3'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 3)
+  end
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles
   end
 
@@ -10,14 +16,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account information was successfully updated!"
-      redirect_to articles_path
+      redirect_to @user
     else    
       render :edit, status: :unprocessable_entity
     end
@@ -26,6 +30,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up!"
       redirect_to articles_path
     else
@@ -34,9 +39,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:success] = "Account and all associated articles successfully deleted"
+    redirect_to root_path
+  end
+
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def require_same_user
+    if current_user != @user && !current_user.admin?
+      flash[:fail] = "You can only edit or delete your own profile"
+      redirect_to @user
+    end
   end
 
 end
